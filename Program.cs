4694c2386a6b23a785106677ef8b0320d306f2b5
Michael Yeaney <michael.yeaney@gmail.com>
2013-09-08 20:16:51 -0400

    Oops...used wrong rate.

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ namespace QueueSimulator
             {
                 // Sample an arrival rate
                 var currentArrivalRate = (int)Math.Round(PoissonDistribution.Next(arrivalRate));
-                var currentServiceRate = (int)Math.Round(arrivalRate);
+                var currentServiceRate = (int)Math.Round(serviceRate);
 
                 // Obey cool-down periods
                 if (!allowArrivals)
