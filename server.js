59a75d4e26b5c5cef698383b17dabc15ea12212b
Michael Yeaney <michael.yeaney@gmail.com>
2015-12-26 22:02:37 -0500

    Added distribution lists to input panel; added gradient to header

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+//
 // Main node entry point. This file will be automatically
 // bootstrapped by the Azure runtime.
+//
 
 // For now just manually bind to a socket.
-var http = require('http')
-var port = process.env.PORT || 1337;
+var express = require('express')
+var app = express();
 
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+// Check for the PORT env var from the azure host
+var port = process.env.PORT || 8008;
+
+// Enable basic static resource support
+app.use(express.static(__dirname, {
+    index : 'default.html'
+}));
+
+// Init server loop
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
